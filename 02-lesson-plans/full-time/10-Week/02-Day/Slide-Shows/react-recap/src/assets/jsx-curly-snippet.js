b8e697f4760d52c0df71573b874e53f365f2530f
dbilenkin <dbilenkin@gmail.com>
2018-07-24 18:12:40 -0400

    Deleted fulltime

--- 02-lesson-plans/full-time/10-Week/02-Day/Slide-Shows/react-recap/src/assets/jsx-curly-snippet.js
+++
@@ -1,6 +0,0 @@
-module.exports = `const reactIs = "Awesome";
-
-const Hello = () => <h1>React is so {reactIs}</h1>;
-
-ReactDOM.render(<Hello />, mountNode);
-`;
--- 02-lesson-plans/full-time/10-Week/02-Day/Slide-Shows/react-recap/src/assets/props-snippet.js
+++
@@ -1,9 +0,0 @@
-module.exports = `const Alert = props =>
-  <div className={\`alert alert-\${props.type || "info"}\`}>
-    {props.children}
-  </div>;
-
-const App = () => <Alert type="danger">Invalid Password</Alert>;
-
-ReactDOM.render(<App />, mountNode);
-`;
